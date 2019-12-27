36cdeaf209578ae62b28d5c469b06aea7130849c
ollinieminen <otn987@gmail.com>
2019-12-27 00:32:28 -0800

    feat:add gpio driver

---
+++ gpioDriver.ts
@@ -0,0 +1,29 @@
+const Gpio = require("pi-gpio"); var gpioPin = 7;
+
+export default class GpioDriver {
+  fan: any
+  ignitor: any
+  auger: any
+
+  constructor() {
+    this.fan = new Gpio(25)
+    this.ignitor = new Gpio(8)
+    this.auger = new Gpio(7)
+  }
+
+  pinStatus = (err, val) => {
+    if (val) {
+      console.log(val)
+    } else {
+      console.log(err)
+    }
+
+  }
+
+  ignitionControl() {
+    this.ignitor.writeSync(turnOn())
+    this.ignitor.read(this.pinStatus)
+  }
+
+
+}
