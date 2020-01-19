94974fb72e2ad54bd022d10f29cb9cff68ef1cb6
ollinieminen <otn987@gmail.com>
2020-01-19 14:17:57 -0800

    test

--- x_state/xstate.js
+++ x_state/xstate.js
@@ -15,7 +15,10 @@ const stateMachine = {
       startGrill: {
         invoke:{
           id: 'startGrill',
-          src: (context, event) => { return new Promise((res, rej)=>{setTimeout(()=>{()=>res.send('hi')}, 8000)})},
+          src: (context, event) => { return new Promise((res, rej)=>{
+            const done = ()=>res.send('done');
+            setTimeout(done, 8000);
+          })},
           onDone: [
             {target: 'smoke', cond: context => context.targetMode === 'smoke'},
             {targert: 'grill', cond: context => context.targetMode == 'grill'},
