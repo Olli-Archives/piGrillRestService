51bd3b1b7e2fb5f167a0eeca63b2d3cadf2121d6
ollinieminen <otn987@gmail.com>
2020-01-18 22:45:06 -0800

    test

--- gpioFunctions.js
+++ gpioFunctions.js
@@ -1,9 +1,9 @@
 const Gpio = require('onoff').Gpio;
 
-const fan = new Gpio(14, 'in');
-const igniter = new Gpio(15, 'in');
-const auger = new Gpio(18, 'in');
-const rando = new Gpio(25, 'in');
+const fan = new Gpio(14, 'high');
+const igniter = new Gpio(15, 'high');
+const auger = new Gpio(18, 'high');
+const rando = new Gpio(25, 'high');
 
 const gpioShutdown = ()=>{
   // TODO: check successfull writeSync
